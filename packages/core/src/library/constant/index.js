504de5290d272e76b37ea815f54b723ca31c8839
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-20 14:49:55 +0200

    implement library constant

---
+++ packages/core/src/library/constant/index.js
@@ -0,0 +1,16 @@
+export const placeholderURL = Object.freeze('https://via.placeholder.com/300')
+
+export const keyCode = Object.freeze({
+  enter: 13,
+})
+
+export const defaultBreed = Object.freeze({
+  dog: 'collie',
+  cat: 'beng',
+  unknown: 'unknown',
+})
+
+export const httpResponseCode = Object.freeze({
+  success: 200,
+  notFound: 404,
+})
