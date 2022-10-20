5c2288d7b977b1ce16fbed50224709ba25dbb329
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-20 15:03:39 +0200

    align isRunningOnGitHubPages

--- packages/core/src/library/environment/host/index.js
+++ packages/core/src/library/environment/host/index.js
@@ -2,4 +2,4 @@ export const isRunningOnLocalHostViaPort = process.env.PORT === '3000'
 
 export const isRunningOnLocalHostViaDomain = typeof window !== 'undefined' && window.location.href.includes('localhost')
 
-export const isRunningOnGitHubPages = process.env.BASE_URL === '/fable' && window.location.href.includes('mts.github.io/fable')
+export const isRunningOnGitHubPages = process.env.BASE_URL === '/fable' && window.location.href.includes('papillonbits.github.io/fable/')
